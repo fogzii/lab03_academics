38daabe8776880428567862ccfbf5363226963c2
Khiet Tam Nguyen <nktnet999@gmail.com>
2022-09-04 17:23:58 +1000

    Ready for 22T3

---
+++ academics.js
@@ -0,0 +1,150 @@
+/**
+ * @module academics
+ */
+
+/**
+ * Create your dataStore here. The design is entirely up to you!
+ * One possible starting point is
+ *
+ * let/const dataStore = {
+ *   academics: [],
+ *   courses: []
+ * }
+ *
+ * and adding to the dataStore the necessary information when the
+ * "register" functions are used.
+ *
+ * You will also need to modify the clear function accordingly
+ * - we recommend you complete clear() at the bottom first!
+ * 
+ * Do not export the dataStore. Your tests should not use/rely on
+ * how dataStore is structured - only what goes in and out of the
+ * defined functions from the interface.
+ */
+
+// TODO
+
+/**
+ * Complete the functions from the interface table.
+ * As an optional activity, you can document your functions
+ * similar to how academicCreate has been done.
+ *
+ * A reminder to return { error: 'any relevant error message of your choice' }
+ * for error cases.
+ */
+
+/**
+ * Creates a new academic, returning an object containing
+ * a unique academic id
+ *
+ * @param {string} name
+ * @param {string} hobby
+ * @returns {{academicId: number}}
+ */
+export function academicCreate(name, hobby) {
+  // TODO:
+  return {
+    academicId: 111,
+  };
+}
+
+/**
+ * Some description
+ *
+ * @param {number} academicId
+ * @param {string} name
+ * @param {string} description
+ * @returns {{courseId: number}}
+ */
+export function courseCreate(academicId, name, description) {
+  // TODO
+  return {
+    courseId: 123,
+  };
+}
+
+/**
+ * Some documentation
+ */
+export function academicDetails(academicId, academicToViewId) {
+  // TODO
+return {
+  academic: {
+    academicId: -999,
+    name: 'Aya',
+    hobby: 'music',
+  }
+};
+}
+
+export function courseDetails(academicId, courseId) {
+  // TODO
+  return {
+    course: {
+      courseId: -1531,
+      name: 'COMP1531',
+      description: 'Software Engineering Fundamentals',
+      staffMembers: [
+        {
+          academicId: -999,
+          name: 'Ben',
+          hobby: 'boxing',
+        },
+      ],
+      allMembers: [
+        {
+          academicId: -999,
+          name: 'Ben',
+          hobby: 'boxing',
+        },
+        {
+          academicId: -888,
+          name: 'Cid',
+          hobby: 'novel',
+        },
+      ],
+    }
+  };
+}
+
+export function academicsList(academicId) {
+  // TODO
+  return {
+    academics: [
+      {
+        academicId: 111,
+        academicName: 'Dan',
+      },
+      {
+        academicId: 222,
+        academicName: 'Eve',
+      },
+    ]
+  };
+}
+
+export function coursesList(academicId) {
+  // TODO
+  return {
+    courses: [
+      {
+        courseId: 123,
+        courseName: 'Course 1',
+      },
+      {
+        courseId: 234,
+        courseName: 'Course 2',
+      },
+    ]
+  };
+}
+
+export function courseEnrol(academicId, courseId, isStaff) {
+  // TODO
+  return {};
+}
+
+export function clear() {
+  // TODO
+  return {};
+}
---
+++ academics.test.js
@@ -0,0 +1,77 @@
+/**
+ * NOTE: The only functions that you should write tests for are those defined
+ * in the specification's interface (README.md). 
+ * 
+ * Your dataStore or any "helper" function you define should NOT be imported or
+ * tested - your tests must work even if it is run against another student's
+ * solution to this lab.
+ */
+
+import {
+  academicCreate,
+  courseCreate,
+  academicDetails,
+  courseDetails,
+  academicsList,
+  coursesList,
+  courseEnrol,
+  clear,
+} from './academics';
+
+test('Remove this test and uncomment the sample tests further below', () => {
+  expect(1 + 1).toEqual(2);
+});
+
+/*
+
+// FIXME
+// This is a sample test that tests many academic functions together
+// You may want to break this up into multiple tests.
+describe('Sample test', () => {
+  test('error creating academics', () => {
+    // Every test should be indendent of other tests. This can be achieve by
+    // clearing and reinitialising the database.
+    // You may want to look at Jest's beforeEach and afterEach.
+    clear();
+
+    // Empty name
+    expect(academicCreate('', 'dancing')).toStrictEqual({ error: expect.any(String) });
+  });
+
+  test('correct return type', ()=> {
+    // Consider beforeEach
+    clear();
+
+    const academic = academicCreate('Magnus', 'chess');
+
+    // NOTE: We don't actually know what the generated ID should be
+    expect(academic).toStrictEqual(
+      {
+        academicId: expect.any(Number),
+      }
+    );
+
+    // However, we can still use this ID in other functions
+    expect(academicDetails(academic.academicId, academic.academicId)).toStrictEqual({
+      academic: {
+        academicId: academic.academicId,
+        name: 'Magnus',
+        hobby: 'chess',
+      }
+    });
+
+    // Note the different key for "name" in this function - refer to "Data Types"
+    // When comparing arrays with multiple items, you may want to convert each
+    // array into a Set (since we don't know which order the items will be in).
+    expect(academicsList(academic.academicId)).toStrictEqual({
+      academics: [
+        {
+          academicId: academic.academicId,
+          academicName: 'Magnus',
+        }
+      ]
+    });
+  });
+});
+
+*/
