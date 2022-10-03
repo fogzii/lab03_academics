4e62f90acbdc5f85f4f5ae0267432055c067de1e
Bowen Xiao <z5416195@ad.unsw.edu.au>
2022-10-04 01:27:49 +1100

    drafted tests for academics.js

--- academics.test.js
+++ academics.test.js
@@ -18,29 +18,222 @@ import {
   clear,
 } from './academics';
 
-test('Remove this test and uncomment the sample tests further below', () => {
-  expect(1 + 1).toEqual(2);
-});
+// resets database before each test
+beforeEach(() => clear());
 
-/*
+// begin tests
+describe('Functions that create/add items', () => {
 
-// FIXME
-// This is a sample test that tests many academic functions together
-// You may want to break this up into multiple tests.
-describe('Sample test', () => {
-  test('error creating academics', () => {
-    // Each test should be independent of other tests. This can be achieve by
-    // clearing and reinitialising the database.
-    // You may want to look at Jest's beforeEach and afterEach.
-    clear();
-
-    // Empty name
-    expect(academicCreate('', 'dancing')).toStrictEqual({ error: expect.any(String) });
+  test('successful creation of an academic', () => {
+    expect(academicCreate('Justin', 'pole dancing')).toStrictEqual(
+      { 
+        academicId: expect.any(Number),
+      }
+    );
+  });
+  
+  test('unsuccessful creation of an academic due to empty name', () => {
+    expect(academicCreate('', 'pole dancing')).toStrictEqual({ error: expect.any(String) });
   });
 
-  test('correct return type', ()=> {
-    // Consider beforeEach
-    clear();
+  test('unsuccessful creation of an academic due to empty hobby', () => {
+    expect(academicCreate('Justin', '')).toStrictEqual({ error: expect.any(String) });
+  });
+
+  test('unsuccessful creation of an academic due to empty name & hobby', () => {
+    expect(academicCreate('', '')).toStrictEqual({ error: expect.any(String) });
+  });
+
+  test('successful creation of a course', ()=> { // check ater that creator is staff and member
+    const academic = academicCreate('Justin', 'pole dancing');
+    expect(courseCreate(academic.academicId, 'POLE1101', 'pole dancing fundamentals')).toStrictEqual(
+      {
+        courseId: expect.any(Number),
+      }
+    );
+  });
+
+  test('unsuccessful creation of a course due to invalid id', ()=> {
+    expect(courseCreate(777, 'POLE1101', 'pole dancing fundamentals')).toStrictEqual({ error: expect.any(String) });
+  });
+
+  test('unsuccessful creation of a course due to invalid name', ()=> {
+    const academic = academicCreate('Justin', 'pole dancing');
+    expect(courseCreate(academic.academicId, 'POLEDANCING1101', 'pole dancing fundamentals')).toStrictEqual({ error: expect.any(String) });
+    expect(courseCreate(academic.academicId, '', '')).toStrictEqual({ error: expect.any(String) });
+  });
+
+  test('successful course enrolment as member', ()=> {
+    const academic = academicCreate('Justin', 'pole dancing');
+    const academic2 = academicCreate('Peter', 'tutoring');
+    const course = courseCreate(academic.academicId, 'POLE1101', 'pole dancing fundamentals');
+
+    courseEnrol(academic2.academicId, course.courseId, false);
+    expect(courseDetails(academic.academicId, course.courseId).allMembers).toStrictEqual({
+      course: {
+        courseId: course.courseId,
+        name: 'POLE1101',
+        description: 'pole dancing fundamentals',
+        allMembers: [academicDetails(academic.academicId, academic.academicId), academicDetails(academic2.academicId, academic2.academicId)],
+        staffMembers: [academicDetails(academic.academicId, academic.academicId)],
+      }
+    });
+  });
+
+  test('successful course enrolment as staff', ()=> {
+    const academic = academicCreate('Justin', 'pole dancing');
+    const academic2 = academicCreate('Peter', 'tutoring');
+    const course = courseCreate(academic.academicId, 'POLE1101', 'pole dancing fundamentals');
+
+    courseEnrol(academic2.academicId, course.courseId, true);
+    expect(courseDetails(academic.academicId, course.courseId).allMembers).toStrictEqual({
+      course: {
+        courseId: course.courseId,
+        name: 'POLE1101',
+        description: 'pole dancing fundamentals',
+        allMembers: [academicDetails(academic.academicId, academic.academicId)],
+        staffMembers: [academicDetails(academic.academicId, academic.academicId), academicDetails(academic2.academicId, academic2.academicId)],
+      }
+    });
+  });
+
+  test('unsuccessful course enrolment', ()=> {
+    const academic = academicCreate('Justin', 'pole dancing');
+    const academic2 = academicCreate('Peter', 'tutoring');
+    const course = courseCreate(academic.academicId, 'POLE1101', 'pole dancing fundamentals');
+    courseEnrol(academic2.academicId, course.courseId, false);
+
+    // invalid academicId
+    expect(courseEnrol(academic2.academicId + 1, course.courseId, true)).toStrictEqual({ error: expect.any(String) });
+
+    // invalid courseId
+    expect(courseEnrol(academic2.academicId, course.courseId + 1, true)).toStrictEqual({ error: expect.any(String) });
+
+    // academic is staff already
+    expect(courseEnrol(academic.academicId, course.courseId, true)).toStrictEqual({ error: expect.any(String) });
+
+    // academic is member already
+    expect(courseEnrol(academic2.academicId, course.courseId, false)).toStrictEqual({ error: expect.any(String) });
+    expect(courseEnrol(academic2.academicId, course.courseId, true)).toStrictEqual({ error: expect.any(String) });
+  });
+});
+
+describe('Functions that request details', () => {
+  test('successful academic return', ()=> {
+    const academic = academicCreate('Justin', 'pole dancing');
+    expect(academicDetails(academic.academicId, academic.academicId)).toStrictEqual({
+      academic: {
+        academicId: academic.academicId,
+        name: 'Justin',
+        hobby: 'pole dancing', 
+      }
+    });
+  });
+
+  test('unsuccessful academic return', ()=> {
+    // invalid academicId and academicToViewId
+    expect(academicDetails(777, 777)).toStrictEqual({ error: expect.any(String) });
+
+    const academic = academicCreate('Justin', 'pole dancing');
+    // invalid academicToViewId
+    expect(academicDetails(academic.academicId, academic.academicId + 1)).toStrictEqual({ error: expect.any(String) });
+
+    // invalid academicId
+    expect(academicDetails(academic.academicId + 1, academic.academicId)).toStrictEqual({ error: expect.any(String) });
+
+  });
+
+  test('successful course return', ()=> {
+    const academic = academicCreate('Justin', 'pole dancing');
+    const course = courseCreate(academic.academicId, 'POLE1101', 'pole dancing fundamentals');
+    expect(courseDetails(academic.academicId, course.courseId)).toStrictEqual({
+      course: {
+        courseId: course.courseId,
+        name: 'POLE1101',
+        description: 'pole dancing fundamentals',
+        allMembers: [academicDetails(academic.academicId, academic.academicId)],
+        staffMembers: [academicDetails(academic.academicId, academic.academicId)],
+      }
+    });
+  });
+
+  test('unsuccessful course return', ()=> {
+    const academic = academicCreate('Justin', 'pole dancing');
+    const course = courseCreate(academic.academicId, 'POLE1101', 'pole dancing fundamentals');
+
+    // invalid academicId
+    expect(courseDetails(academic.academicId + 1, course.courseId)).toStrictEqual({ error: expect.any(String) });
+    
+    // invalid courseId
+    expect(courseDetails(academic.academicId, course.courseId + 1)).toStrictEqual({ error: expect.any(String) });
+
+    // invalid academicId and courseId
+    expect(courseDetails(academic.academicId + 1, course.courseId + 1)).toStrictEqual({ error: expect.any(String) });
+
+    // academicId refers to an academic not in the specified course
+    const academic2 = academicCreate('Peter', 'tutoring');
+    expect(courseDetails(academic2.academicId, course.courseId)).toStrictEqual({ error: expect.any(String) });
+  });
+
+  test('successful list of academics return', ()=> {
+    const academic = academicCreate('Justin', 'pole dancing');
+    const academic2 = academicCreate('Peter', 'tutoring');
+
+    const listAcademics = new Set();
+    listAcademics.add(
+      {
+        academicId: academic.academicId, 
+        academicName: academicDetails(academic.academicId, academic.academicId).name,
+      }
+    );
+    listAcademics.add(
+      {
+        academicId: academic2.academicId, 
+        academicName: academicDetails(academic2.academicId, academic2.academicId).name,
+      }
+    );
+
+    const list = academicsList(academic.academicId);
+    const listAcademicsCompare = new Set(list);
+
+    expect(listAcademics).toStrictEqual(listAcademicsCompare);
+  });
+
+  test('unsuccessful list of academics return', ()=> {
+    expect(academicsList(777)).toStrictEqual({ error: expect.any(String) });
+  });
+
+  test('successful list of courses return', ()=> {
+    const academic = academicCreate('Justin', 'pole dancing');
+    const course = courseCreate(academic.academicId, 'POLE1101', 'pole dancing fundamentals');
+    const academic2 = academicCreate('Peter', 'tutoring');
+    const course2 = courseCreate(academic2.academicId, 'TUTE1101', 'tutoring fundamentals');
+
+    const listCourses = new Set();
+    listCourses.add(
+      {
+        courseId: course.courseId, 
+        courseName: courseDetails(academic.academicId, course.courseId).name,
+      }
+    );
+    listCourses.add(
+      {
+        courseId: course.courseId, 
+        courseName: courseDetails(academic2.academicId, course2.courseId).name,
+      }
+    );
+
+    const list = CoursesList(academic.academicId);
+    const listCoursesCompare = new Set(list);
+
+    expect(listCourses).toStrictEqual(listCoursesCompare);
+  });
+
+  test('unsuccessful list of courses return', ()=> {
+    expect(coursesList(777)).toStrictEqual({ error: expect.any(String) });
+  });
+});
+
 
     const academic = academicCreate('Magnus', 'chess');
 
@@ -71,7 +264,3 @@ describe('Sample test', () => {
         }
       ]
     });
-  });
-});
-
-*/
